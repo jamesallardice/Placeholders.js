f178b83235a6e88419dd7877fea568f9eb1c2eaa
James Allardice <jim.allardice@gmail.com>
2013-03-20 16:25:26 +0000

    Added hide-on-input functionality to v2

---
+++ test/focus/tests.js
@@ -0,0 +1,28 @@
+var form = document.getElementById("form1"),
+    newInput = document.createElement("input");
+
+setTimeout(function () {
+
+    // Test changing placeholder attribute value
+    document.getElementById("handle1").setAttribute("placeholder", "This value has changed");
+
+    // Test created placeholder attribute value
+    document.getElementById("handle2").setAttribute("placeholder", "This value has been added");
+
+    // Test new input element added to the DOM after page load
+    newInput.setAttribute("type", "text");
+    newInput.setAttribute("placeholder", "Test created element");
+    document.body.appendChild(newInput);
+
+}, 1000);
+
+// Bind a submit event handler to the test form
+function submitHandler(e) {
+    e.returnValue = false;
+    return false;
+}
+if (form.addEventListener) {
+    form.addEventListener("submit", submitHandler);
+} else if (form.attachEvent) {
+    form.attachEvent("onsubmit", submitHandler);
+}
