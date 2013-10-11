45c24bddf3b27c358067550d5d18f38c1b4db176
James Allardice <jim.allardice@gmail.com>
2013-10-11 07:48:23 +0100

    Improved jQuery adapter tests and added tests for .prop() method patch

--- test/adapters/jquery/tests.js
+++ test/adapters/jquery/tests.js
@@ -14,15 +14,26 @@ setTimeout(function () {
     }
 
     setTimeout(function () {
-        // The behaviour of `val` as a getter should be patched
-        alert($("#jq1").val());
-        alert($("#jq2").val());
-        alert($("#handle1").val());
-        alert($("#handle2").val());
-        alert($("#handle3").val());
 
-        // The behaviour of `val` as a setter should not be affected)
+        // The behaviour of `val` and `prop` as getters should be patched
+        alert($("#jq1").val() === "");
+        alert($("#jq1").prop("value") === "");
+
+        alert($("#jq2").val() === "Initial value");
+        alert($("#jq2").prop("value") === "Initial value");
+
+        alert($("#handle1").val() === "");
+        alert($("#handle1").prop("value") === "");
+
+        alert($("#handle2").val() === "");
+        alert($("#handle2").prop("value") === "");
+
+        alert($("#handle3").val() === "");
+        alert($("#handle3").prop("value") === "");
+
+        // The behaviour of `val` and `prop` as setters should not be affected)
         alert($("#jq1").val("set new") instanceof $);
+        alert($("#handle3").prop("value", "another new") instanceof $);
     }, 1000);
 
 }, 1000);
