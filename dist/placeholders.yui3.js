c97ee3c45cd7788e572bb9f24271dda67dfa283b
James Allardice <james.allardice@capita.co.uk>
2014-03-19 12:59:28 +0000

    Add built files for Bower

---
+++ dist/placeholders.yui3.js
@@ -0,0 +1,21 @@
+(function (YUI) {
+
+    /*jshint yui: true */
+
+    "use strict";
+
+    YUI.add("placeholders", function (Y) {
+        var originalGetFn = Y.Node.prototype.get;
+        Y.Node.prototype.get = function (attr) {
+            if (attr === "value" && this.getAttribute("data-placeholder-active")) {
+                return "";
+            }
+            return originalGetFn.apply(this, arguments);
+        };
+    }, "3.0.0", {
+        requires: [
+            "node"
+        ]
+    });
+
+}(YUI));
