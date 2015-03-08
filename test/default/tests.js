b29d94260c98b5dae80e04a75e0ffe0e1e145f56
James Allardice <jim.allardice@gmail.com>
2015-03-08 12:28:35 +0000

    Bring test files in line with new coding style

--- test/default/tests.js
+++ test/default/tests.js
@@ -1,37 +1,42 @@
-var form = document.getElementById("form1"),
-    newInput = document.createElement("input");
+var form = document.getElementById('form1');
+var newInput = document.createElement('input');
 
 setTimeout(function () {
 
-    // Test changing placeholder attribute value
-    document.getElementById("handle1").setAttribute("placeholder", "This value has changed");
+  // Test changing placeholder attribute value
+  document
+  .getElementById('handle1')
+  .setAttribute('placeholder', 'This value has changed');
 
-    // Test created placeholder attribute value
-    document.getElementById("handle2").setAttribute("placeholder", "This value has been added");
+  // Test created placeholder attribute value
+  document
+  .getElementById('handle2')
+  .setAttribute('placeholder', 'This value has been added');
 
-    // Test new input element added to the DOM after page load
-    newInput.setAttribute("type", "text");
-    newInput.setAttribute("placeholder", "Test created element");
-    document.body.appendChild(newInput);
+  // Test new input element added to the DOM after page load
+  newInput.setAttribute('type', 'text');
+  newInput.setAttribute('placeholder', 'Test created element');
+  document.body.appendChild(newInput);
 
-    // Test input type changing after page load
-    try {
-        document.getElementById("handle3").type = "password";
-    } catch (e) {
-        // This will fail in IE < 9
-    }
+  // Test input type changing after page load
+  try {
+    document.getElementById('handle3').type = 'password';
+  } catch ( e ) {
+    // This will fail in IE < 9
+  }
 
-    document.getElementById("handle4").removeAttribute("placeholder");
+  document.getElementById('handle4').removeAttribute('placeholder');
 
 }, 1000);
 
 // Bind a submit event handler to the test form
-function submitHandler(e) {
-    e.returnValue = false;
-    return false;
+function submitHandler( e ) {
+  e.returnValue = false;
+  return false;
 }
-if (form.addEventListener) {
-    form.addEventListener("submit", submitHandler);
-} else if (form.attachEvent) {
-    form.attachEvent("onsubmit", submitHandler);
+
+if ( form.addEventListener ) {
+  form.addEventListener('submit', submitHandler);
+} else if ( form.attachEvent ) {
+  form.attachEvent('onsubmit', submitHandler);
 }
